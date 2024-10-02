e9ceaac1e89ad25a54ad29766bf94fd8495ab2f1
adam n <3193391+bcuz@users.noreply.github.com>
2024-10-02 16:30:10 -0400

    done

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function (req, res) {
+  const userAgent = req.headers['user-agent'];
+  const language = req.headers['accept-language'];
+  // res.send(req.headers)
+  res.json({ipaddress: req.ip, software: userAgent, language})
+  // req.ip
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
